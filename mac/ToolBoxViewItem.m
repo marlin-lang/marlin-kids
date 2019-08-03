77d0f7fa4fa983102eedfd53d5e26482f55c9a0a
Yongwu <codingpotato@gmail.com>
2019-08-03 09:33:43 +0800

    show let in tool box

---
+++ mac/ToolBoxViewItem.m
@@ -0,0 +1,21 @@
+//
+//  ToolBoxViewItem.m
+//  marlin-kids.mac
+//
+//  Created by wangyw on 2019/8/3.
+//
+
+#import "ToolBoxViewItem.h"
+
+@interface ToolBoxViewItem ()
+
+@end
+
+@implementation ToolBoxViewItem
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
