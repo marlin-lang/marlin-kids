f673d66ae0ff33948d2f0f2d75554e930136e0bb
codingpotato <codingpotato@gmail.com>
2019-08-08 07:47:45 +0800

    Use formatter to limit input

--- mac/EditorViewController.mm
+++ mac/EditorViewController.mm
@@ -1,5 +1,36 @@
 #import "EditorViewController.h"
 
+@interface TestFormater : NSFormatter
+
+@end
+
+@implementation TestFormater
+
+- (NSString *)stringForObjectValue:(id)obj {
+  return @"";
+}
+
+- (BOOL)getObjectValue:(out id _Nullable __autoreleasing *)obj
+             forString:(NSString *)string
+      errorDescription:(out NSString *__autoreleasing _Nullable *)error {
+  return YES;
+}
+
+- (BOOL)isPartialStringValid:(NSString *)partialString
+            newEditingString:(NSString *__autoreleasing _Nullable *)newString
+            errorDescription:(NSString *__autoreleasing _Nullable *)error {
+  for (auto i = 0; i < partialString.length; ++i) {
+    auto ch = [partialString characterAtIndex:i];
+    if (ch < '0' || ch > '9') {
+      NSBeep();
+      return NO;
+    }
+  }
+  return YES;
+}
+
+@end
+
 @interface EditorViewController ()
 
 @property(weak) IBOutlet NSTextField *editorTextField;
@@ -10,6 +41,9 @@
 
 - (void)viewDidLoad {
   [super viewDidLoad];
+
+  self.editorTextField.cell.formatter = [TestFormater new];
+  self.editorTextField.placeholderString = @"Please input number";
 }
 
 - (IBAction)okButtonPressed:(id)sender {
